1589463ffb1152f12ec224dd435ca70aacc42c64
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-05-30 11:18:47 +0700

    update code.

--- src/UserResult.kt
+++ src/UserResult.kt
@@ -6,8 +6,11 @@ fun retrieveUsers(): UserResult {
     return Success(usersFromJSONFile("user.json"))
 }
 
+fun retrieveUsersFailure(): UserResult {
+    return Failure("error")
+}
+
 fun main(args: Array<String>) {
-    val result = retrieveUsers()
 
     val value = generateSequence(1) {
         it * 10
@@ -18,9 +21,12 @@ fun main(args: Array<String>) {
     val users = usersFromJSONFile("user.json").asSequence()
     users.forEach { println(it.username) }
 
+    users.take(users.count()).forEach { println(it.id + 1) }
+
     users.map { user -> user.email.replace(user.email, "test@test.test") }
             .forEach { println(it) }
 
+    val result = retrieveUsers()
     when (result) {
         is Success -> result.users.forEach({ println(it.email) })
         is Failure -> print(result.message)
